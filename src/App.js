fd16eb5e27ad72b2e5136937d78d411388847a94
Guilherme777 <98350315+JoaoGuilherme777@users.noreply.github.com>
2022-11-29 16:19:51 -0300

    29/11

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import { Container, Content } from './styles'
+import Input from './components/Input'
+//import Button from './components/Button/'
+const App = () => {
+  return (
+    <Container>
+      <Content>
+      <Input />
+      </Content>
+    </Container>
+  );
+}
+
+export default App;
